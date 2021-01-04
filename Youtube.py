13fdb7b47bbfdac020f302c55d90a0fe63b36f57
Manucho Adero <42127278+manucho254@users.noreply.github.com>
2021-01-04 21:40:38 +0300

    Better Formatting

--- Youtube.py
+++ Youtube.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 
 root = Tk()
 
-winicon = PhotoImage(file = 'cool.png')
+winicon = PhotoImage(file='cool.png')
 
 root.iconphoto(False, winicon)
 root.configure(bg='#2B547E')
@@ -15,67 +15,72 @@ root.title("YOUTUBE DOWNLOADER")
 root.geometry('450x400')
 root.resizable(width=0, height=0)
 
-label = Label(root,text = "YOUTUBE DOWNLOADER",bg='red',fg='white',
-              font = "bold" , width="40", borderwidth=10 ,padx=10, pady=13
+label = Label(root, text="YOUTUBE DOWNLOADER", bg='red', fg='white',
+              font="bold", width="40", borderwidth=10, padx=10, pady=13
               )
-label.grid(row=0,columnspan=3, padx=10, pady=10)
-label.place(x = 20,
-          y = 10)
+label.grid(row=0, columnspan=3, padx=10, pady=10)
+label.place(x=20,
+            y=10)
 
 
-label1 = Label(root, text = "Download 1 Video",
-              font = "bold",
-              bg = "black", fg="white",width=30
-              )
-label1.place(x = 90,
-          y = 85)
+label1 = Label(root, text="Download Video:",
+               font="bold",
+               bg="black", fg="white", width=30
+               )
+label1.place(x=90,
+             y=85)
 
 tube_entry1 = StringVar()
-entry1 = Entry(root, width = 45,  
-              bd = 6, font = 14, borderwidth=6, textvariable=tube_entry1)
-entry1.insert(0,"Youtube Video Link")
+entry1 = Entry(root, width=45,
+               bd=6, font=14, borderwidth=6, textvariable=tube_entry1)
+entry1.insert(0, "Youtube Video Link")
 entry1.grid(row=1, column=1, columnspan=1, padx=10, pady=13)
 
-entry1.place(x = 10,
-          y = 120)
+entry1.place(x=10,
+             y=120)
 
-#single download function
+# single download function
+
+
 def download_single():
     url = entry1.get()
     try:
-       YouTube(url).streams.first().download()
-       yt = YouTube(url)
-       yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-       if yt == True:
-           messagebox.showinfo("Download started")
+        YouTube(url).streams.first().download()
+        yt = YouTube(url)
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+            'resolution').desc().first().download()
+        messagebox.showinfo("Success", "Video Downloaded")
     except:
         messagebox.showerror("Error", "Please insert Url.!!")
 
+
 button_single = Button(
-    root, text="DOWNLOAD" ,
-    padx=30, pady=10, 
-    bg = 'red', fg='white', command=download_single
-    )
+    root, text="DOWNLOAD",
+    padx=30, pady=10,
+    bg='red', fg='white', command=download_single
+)
 
-button_single.place(x = 150,
-          y = 165)
+button_single.place(x=150,
+                    y=165)
 
-label2 = Label(root, text = "Download Playlist",
-              font = "bold",
-              bg = "black", fg="white",width=30
-              )
-label2.place(x = 90,
-          y = 217)
+label2 = Label(root, text="Download Playlist:",
+               font="bold",
+               bg="black", fg="white", width=30
+               )
+label2.place(x=90,
+             y=217)
 
 tube_entry2 = StringVar()
-entry2 = Entry(root, width = 45,  
-              bd = 6, font = 14, borderwidth=6, textvariable=tube_entry2)
-entry2.insert(0,"Youtube Playlist Link")
+entry2 = Entry(root, width=45,
+               bd=6, font=14, borderwidth=6, textvariable=tube_entry2)
+entry2.insert(0, "Youtube Playlist Link")
 entry2.grid(row=2, column=1, columnspan=1, padx=10, pady=13)
-entry2.place(x = 10,
-          y = 250)
+entry2.place(x=10,
+             y=250)
 
 # multi download function
+
+
 def download_playlist():
     url = entry2.get()
     try:
@@ -85,31 +90,37 @@ def download_playlist():
     except:
         messagebox.showerror("Error", "Please insert Url.!!")
 
-button_playlist = Button(
-    root, text="DOWNLOAD" ,
-    padx=30, pady=10, 
-    bg = 'red', fg="white", command=download_playlist
-    )
 
-button_playlist.place(x = 150,
-          y = 300)
+button_playlist = Button(
+    root, text="DOWNLOAD",
+    padx=30, pady=10,
+    bg='red', fg="white", command=download_playlist
+)
+
+button_playlist.place(x=150,
+                      y=300)
 
 # exit function
+
+
 def button_exit():
-    MsgBox = messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
+    MsgBox = messagebox.askquestion(
+        'Exit Application', 'Are you sure you want to exit the application', icon='warning')
     if MsgBox == 'yes':
-       root.destroy()
+        root.destroy()
     else:
-        messagebox.showinfo('Return','You will now return to the application screen')
+        messagebox.showinfo(
+            'Return', 'You will now return to the application screen')
     return
 
-button_exit = Button(
-    root, text="EXIT" ,
-    padx=30, pady=10,
-    bg = 'red', fg="black", command=button_exit
-    )
 
-button_exit.place(x = 175,
-          y = 350)
+button_exit = Button(
+    root, text="EXIT",
+    padx=30, pady=10,
+    bg='red', fg="black", command=button_exit
+)
+
+button_exit.place(x=175,
+                  y=350)
 
 root.mainloop()
